7c709f135309b603b4044eedf87d86a854674954
ARakhmetkali <ay_rakhmetkali@kbtu.kz>
2023-01-28 13:15:28 +0600

    Python Variables

---
+++ Lab1/c.py
@@ -0,0 +1,28 @@
+#Create a variable name
+ayan = "smart"
+
+
+x = 50
+
+
+a = 10
+b = 5
+print(a+b)
+
+
+x = 10
+y = 20
+z = x +y
+print(z)
+
+
+# 2my-first_name = "John"  <- wrong
+myfirst_name = "John"   # <- True
+
+
+x = y = z = "Orange"
+
+#Вставьте правильное ключевое слово, чтобы переменная x принадлежала глобальной области видимости.
+def myfunc():
+  global x
+  x = "fantastic"
