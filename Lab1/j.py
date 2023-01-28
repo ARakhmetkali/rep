2737b4a1ba4c0619dd92efd705db36cc5327100c
ARakhmetkali <ay_rakhmetkali@kbtu.kz>
2023-01-28 14:22:09 +0600

    Python Operators

---
+++ Lab1/j.py
@@ -0,0 +1,22 @@
+#   *
+print(10 * 5)
+
+
+#   /
+print(10 / 2)
+
+
+# if ____ in _____:
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+
+
+#  !=
+if 5 != 10:
+  print("5 and 10 is not equal")
+
+
+#  or
+if 5 == 10 or 4 == 4:
+  print("At least one of the statements is true")
