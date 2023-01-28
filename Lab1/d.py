0f77101f77084a81baab7f0840ac784e977947ec
ARakhmetkali <ay_rakhmetkali@kbtu.kz>
2023-01-28 13:21:22 +0600

    Python Data Types

---
+++ Lab1/d.py
@@ -0,0 +1,33 @@
+#int
+x = 5
+print(type(x))
+
+
+#str
+x = "Hello World"
+print(type(x))
+
+
+#float
+x = 20.5
+print(type(x))
+
+
+#list
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+
+#tuple
+x = ("apple", "banana", "cherry")
+print(type(x))
+
+
+#dict
+x = {"name" : "John", "age" : 36}
+print(type(x))
+
+
+#bool
+x = True
+print(type(x))
