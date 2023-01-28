679de1b47be812bb0f7b36cc17cad228401b7cef
ARakhmetkali <ay_rakhmetkali@kbtu.kz>
2023-01-28 14:49:56 +0600

    Python If...Else

---
+++ Lab1/k.py
@@ -0,0 +1,50 @@
+# a > b
+a = 50
+b = 10
+if a > b:
+    print("Hello World")
+
+
+# !=
+a = 50
+b = 10
+if a != b :
+    print("Hello World")
+
+
+#else
+a = 50
+b = 10
+if a == b:
+    print("Yes")
+else:
+    print("No")
+
+
+#elif
+a = 50
+b = 10
+if a == b:
+    print("1")
+elif a > b:
+    print("2")
+else:
+    print("3")
+
+
+#and
+if a == b and c == d:
+    print("Hello")
+
+
+#or
+if a == b or c == d:
+    print("Hello")
+
+
+#short hand syntax to put the following statement on one line:
+if 5 > 2: print("Five is greater than two!")
+
+
+#short hand syntax to write the following conditional expression in one line:
+print("Yes") if 5 > 2 else print("No")
