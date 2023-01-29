7d6e83c2f0dfbdac7e9395054865ba8f69970d46
ARakhmetkali <ay_rakhmetkali@kbtu.kz>
2023-01-30 00:29:59 +0600

    Python sets

---
+++ Lab2/c.py
@@ -0,0 +1,28 @@
+"""
+set
+"""
+#1.
+fruits = {"apple", "banana", "cherry"}
+if "apple" in fruits:
+    print("Yes, apple is a fruit!")
+
+
+#2. add element( not append)
+fruits = {"apple", "banana", "cherry"}
+fruits.add("orange")
+
+
+#3. method to add multiple items (more_fruits) to the fruits set(update).
+fruits = {"apple", "banana", "cherry"}
+more_fruits = ["orange", "mango", "grapes"]
+fruits.update(more_fruits)
+
+
+#4. remove
+fruits = {"apple", "banana", "cherry"}
+fruits.remove("banana")
+
+
+#5. discard  (to remove "banana" from the fruits set.)
+fruits = {"apple", "banana", "cherry"}
+fruits.discard("banana")
