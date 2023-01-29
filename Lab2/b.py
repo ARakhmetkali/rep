22e2786ce49410a99a49e22a2fc631622e1cd4eb
ARakhmetkali <ay_rakhmetkali@kbtu.kz>
2023-01-30 00:21:00 +0600

    Python tuples

---
+++ Lab2/b.py
@@ -0,0 +1,21 @@
+"""
+TUPLE
+"""
+#1. print the first item
+fruits = ("apple", "banana", "cherry")
+print(fruits[0])
+
+
+#2. number of items
+fruits = ("apple", "banana", "cherry")
+print(len(fruits))
+
+
+#3. negative index to print the last item in the tuple
+fruits = ("apple", "banana", "cherry")
+print(fruits[-1])
+
+
+#4. range of indexes to print the third, fourth, and fifth item in the tuple
+fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(fruits[2:5])
