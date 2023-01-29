743b8e0d369373f7c0ec40e78138702bb01c740b
ARakhmetkali <ay_rakhmetkali@kbtu.kz>
2023-01-30 00:12:26 +0600

    Python lists

---
+++ Lab2/a.py
@@ -0,0 +1,41 @@
+#LIST
+#1. Second item
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+
+
+#2. Changing the value
+fruits = ["apple", "banana", "cherry"]
+fruits[0] = "Kiwi"
+
+
+#3. Using the APPEND to add the value
+fruits = ["apple", "banana", "cherry"]
+fruits.append("pineapple")
+
+
+#4. insert
+fruits = ["apple", "banana", "cherry"] 
+fruits.insert(1, "limon")
+
+
+#5. remove
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+print(fruits)
+
+
+#6. negative index to print the last item in the list
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+#output: cherry
+
+
+#7.  range of index
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(print(fruits[2:5]))
+
+
+#8.  number of items( len() )
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(len(fruits))
