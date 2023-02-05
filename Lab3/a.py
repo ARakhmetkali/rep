891108c22af7b8792f1bb463736b6b06d7fddaf0
ARakhmetkali <ay_rakhmetkali@kbtu.kz>
2023-02-06 00:54:22 +0600

    t

---
+++ Lab3/a.py
@@ -0,0 +1,25 @@
+#Ex1:
+"""
+def my_function():
+    print("Hello from a function")
+
+
+#Ex2: Execute a function named my_function.
+def my_function():
+  print("Hello from a function")
+my_function()
+
+
+#Ex3: Inside a function with two parameters, print the first parameter.
+def my_function(fname, lname):
+    print(fname)
+
+
+#Ex4: return
+def my_function(x):
+    return x + 10
+"""
+
+#Ex5: 
+def my_function(*kids):
+    print("The youngest child is " + kids[2])
