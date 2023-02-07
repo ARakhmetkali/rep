5891fe4edc0e5af49d8027f149e34ab73531a853
ARakhmetkali <ay_rakhmetkali@kbtu.kz>
2023-02-07 23:44:44 +0600

    ex7

--- Ex/7.py
+++ Ex/7.py
@@ -1,9 +1,9 @@
-def has_33():
-    nums = list(map(int, input().split()))
-    for i in range(len(nums)):
-        if nums[i] == 3 and nums[i-1] == 3:
-            print ("True")
-            break
-    else:
-        print ("False")
-has_33()
+def has_33(nums): 
+    for i in range(1,len(nums)): 
+        if nums[i]==3 and nums[i-1]==3: 
+            return True 
+    return False 
+ 
+print(has_33([1, 3, 3])) 
+print(has_33([1, 3, 1, 3])) 
+print(has_33([3, 1, 3]))
