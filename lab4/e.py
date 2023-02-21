03555fcd33a0ed9cf520fb469c0cd5ef0a557c33
ARakhmetkali <ay_rakhmetkali@kbtu.kz>
2023-02-21 20:34:02 +0600

    Python Math

---
+++ lab4/e.py
@@ -0,0 +1,49 @@
+#Python Math
+#1.  min(); max()
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+
+#2. abs()
+x = abs(-7.25)
+
+print(x)
+
+
+#3. pow(x, y) ===> x^y  ===>  the value of 4 to the power of 3 (same as 4 * 4 * 4)
+x = pow(4, 3)
+
+print(x)
+
+
+#4.Math Module
+import math
+
+
+#5. square root
+import math
+
+x = math.sqrt(64)
+
+print(x)
+
+
+#6
+import math
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x) # returns 2
+print(y) # returns 1
+
+
+#7 pi ===> 3.14
+import math
+
+x = math.pi
+
+print(x)
