149dce90f25b2af1548e46a81ab2a569a1bb26b1
ARakhmetkali <ay_rakhmetkali@kbtu.kz>
2023-04-13 14:18:37 +0600

    XD

--- pg.py
+++ pg.py
@@ -1,2 +1,15 @@
 import pygame
-pygame.draw.rect()
+pygame.init()
+weight =500
+hight = 600
+bg = pygame.display.set_mode((500,600))
+pygame.display.set_caption("kNigga")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+    bg.fill((255,255,255))
+    pygame.display.update()
